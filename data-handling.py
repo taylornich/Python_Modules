8b8a55e30a9294d26d07df5b3687c57494baec57
taylornich <taylormanhard15@gmail.com>
2024-07-23 21:57:30 -0700

    Question 1

--- data-handling.py
+++ data-handling.py
@@ -1,4 +1,11 @@
 import mood_responses
 
-mood = input("How are you feeling today? ")
-print(mood_responses.mood_response(mood))
+def main():
+    mood = input("How are you feeling today? ")
+
+    response = mood_responses.mood_response(mood)
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
